5d43831207d4e472ed4ba5c2c8aa19c1b624da1f
Mike Apreza <69696262+MikeA-04@users.noreply.github.com>
2023-07-28 18:12:37 -0500

    Fix adding elements in singly LL

--- singlyLinkedList.js
+++ singlyLinkedList.js
@@ -1,7 +1,7 @@
 // Head of singly linked list
-var head;
+var head = null;
 
-// Implementing a singly linked list
+// Defintion of a node
 class SinglyNode {
     // Constructor
     constructor (val) {
@@ -10,6 +10,7 @@ class SinglyNode {
     }
 }
 
+// This function returns the size of the list.
 function singlySize() {
     if (head == null) {
         return 0;
@@ -25,10 +26,42 @@ function singlySize() {
     }
 }
 
-function add(head, val) {
+// This function prints the values of each node.
+function printSinglyList() {
+    if (head == null) {
+        console.log("null"); 
+    } else {
+        var n = head;
+        var str = "head -> ";
+        // As long as the n value is not null, it can print something
+        while (n) {
+            str = str + String(n.value) + " -> ";
+            n = n.next;
+        }
+        str = str + "null";
+        console.log(str);
+    }
+}
+
+// This funciton accepts a value and adds a new node with that value
+// at the front of the list.
+function addFront(val) {
     // If the list is empty, just create node and set the head
     if (head == null) {
-        head = SinglyNode(val);
+        head = new SinglyNode(val);
+    } else {
+        newHead = new SinglyNode(val);
+        newHead.next = head;
+        head = newHead;
+    }
+}
+
+// This funciton accepts a value and adds a new node with that value
+// at the end of the list.
+function addEnd(val) {
+    // If the list is empty, just create node and set the head
+    if (head == null) {
+        head = new SinglyNode(val);
     } else {
         // Find the end
         var h = head;
@@ -36,22 +69,36 @@ function add(head, val) {
             h = h.next;
         }
         // Add the new node
-        h.next = SinglyNode(val);
+        h.next = new SinglyNode(val);
     }
 }
 
-function addAt(head, val, index) {
+// This function accepts a value and index. It adds a new node with the value 
+// into the list at the specified index.
+function addAt(val, index) {
     // Ensure the index is valid
     const size = singlySize();
-    if (index < size) {
-        // 0 1 2 3, size = 4
-        // Find the valid index
-        var h = head;
-        for (let i = 0; i < size-1; i++) {
-            h = h.next;
+    if (index >= 0 && index < size && head != null) {
+        // If the index is 0, then call the addFront() function
+        if (index == 0) {
+            addFront(val);
+        } else {
+            // Find the previous and next nodes of the ith node
+            var n = head;
+            var prev = null;
+            var i = 0;
+            while (i != index) {
+                prev = n;
+                n = n.next;
+                i++;
+            }
+            var newNode = new SinglyNode(val);
+            newNode.next = n;
+            prev.next = newNode;
         }
     } else {
         // Nothing is changed
+        console.log("New node cannot be inserted because of invalid index or list is empty.");
         return;
     }
 }
